55b15bd093c9bc29e6573d7c3e8bae6739da71b2
Jelani Thomas <eyeglass0324@gmail.com>
2026-04-22 09:13:43 -0400

    API for summarizing reviews created. Generating summaries from DB

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,13 +1,15 @@
 import { adapter } from '../util/prisma.adapter';
 import { PrismaClient, type Review } from '../generated/prisma/client';
+import type { number } from 'zod';
 
 export const reviewRepository = {
-   async getReviews(productId: number): Promise<Review[]> {
+   async getReviews(productId: number, limit?: number): Promise<Review[]> {
       const prisma = new PrismaClient({ adapter });
 
       return prisma.review.findMany({
          where: { productId },
          orderBy: { createAt: 'desc' },
+         take: limit,
       });
    },
 };
