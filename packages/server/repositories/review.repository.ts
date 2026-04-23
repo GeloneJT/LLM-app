cdeefa88f84218db1aa161f3d7de51de9b98273b
Jelani Thomas <eyeglass0324@gmail.com>
2026-04-23 10:32:31 -0400

    summary storing in database implemented

--- packages/server/repositories/review.repository.ts
+++ packages/server/repositories/review.repository.ts
@@ -1,15 +1,32 @@
 import { adapter } from '../util/prisma.adapter';
 import { PrismaClient, type Review } from '../generated/prisma/client';
-import type { number } from 'zod';
+import dayjs from 'dayjs';
+
+const prisma = new PrismaClient({ adapter });
 
 export const reviewRepository = {
    async getReviews(productId: number, limit?: number): Promise<Review[]> {
-      const prisma = new PrismaClient({ adapter });
-
       return prisma.review.findMany({
          where: { productId },
-         orderBy: { createAt: 'desc' },
+         orderBy: { createdAt: 'desc' },
          take: limit,
+      });
+   },
+
+   storeReviewSummary(productId: number, summary: string) {
+      const now = new Date();
+      const expiresAt = dayjs().add(7, 'days').toDate();
+      const data = {
+         content: summary,
+         expiresAt: expiresAt,
+         generatedAt: now,
+         productId,
+      };
+
+      return prisma.summary.upsert({
+         where: { productId },
+         create: data,
+         update: data,
       });
    },
 };
